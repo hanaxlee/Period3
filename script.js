147ae20ba51ac20a4875ff7368bf521640d639a5
hanaxlee <30877949+hanaxlee@users.noreply.github.com>
2017-08-14 10:45:27 -1000

    Add files via upload
    Updated period3.html and script.js files

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function sayHello() {
   //I am creating a function called sayHello//
-  var userName = prompt("What is your name?");
+  var firstName = prompt("What is your first name?");
+  var nickname = prompt("What is your nickname?");
+  var lastName = prompt("What is your last name?");
   //Create a variable called userName and stores the users information//
-  window.alert(userName + ", Hello");
-  //Popup a window that says hello o the user//
+  window.alert(firstName + " " + nickname + " " + lastName);
+  //Popup a window that says their name//
   
 }
