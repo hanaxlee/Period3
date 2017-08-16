cfcd565a14bb90e6e19f96d4d384e0a32d381b86
hanaxlee <30877949+hanaxlee@users.noreply.github.com>
2017-08-16 10:21:50 -1000

    Add files via upload
    array.js

---
+++ array.js
@@ -0,0 +1,8 @@
+var juniors = ["Reimon", "Hana"];
+var seniors = ["Samiyah", "Justin", "Bryson"];
+window.alert("Seniors" + seniors.length + "Juniors" + juniors.length);
+
+//show total number of juniors
+//show total number of seniors
+//Make them all in a single popup window
+//seniors 3 juniors 2
